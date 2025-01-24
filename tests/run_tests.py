9b6146d5fd26c1da39adb67755426d9891404293
DTRHnet <admin@dtrh.net>
2025-01-24 06:58:00 -0500

    Update run_tests.py

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -4,7 +4,7 @@ import unittest
 
 if __name__ == '__main__':
     loader = unittest.TestLoader()
-    start_dir = 'tests'
+    start_dir = './'
     suite = loader.discover(start_dir, pattern='test_*.py')
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(suite)
